8c24091bc052dac4fd5b8557fa3f217ee19746b4
minuk <minuk@192.168.1.176>
2016-09-11 23:04:51 +0900

    add line

--- javabook/src/javabook/ch2/HelloJava.java
+++ javabook/src/javabook/ch2/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("HelloJava");
+		System.out.println("HelloWorld");
 		// TODO Auto-generated method stub
 
 	}
